3797c739920d2af3770d7f67b3aba1dfb2c01332
vivektangudu123 <vivektangudu2383@gmail.com>
2023-10-26 21:42:22 +0530

    hello

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        logger.info("Start building Calculator:");
+        logger.info("Start building:");
         //some changes
         Scanner reader = new Scanner(System.in);
         int op, flag=0;
